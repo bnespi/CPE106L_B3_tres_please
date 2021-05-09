430c9eeefc9531f0379e38261409e59531440534
Rionne Angelo Ugale <angeloaquino285@gmail.com>
2021-05-09 12:42:13 +0800

    put some descriptive comments and suggestions on main

--- main.py
+++ main.py
@@ -27,24 +27,31 @@ def main():
         contactNum = input("Contact Number: ")
         address = input("Address: ")
 
-        client = Person(name, age, contactNum, address)
+        client = Person(name, age, contactNum, address) # new instance of Person object
         
         
         print("\nWhen do you want to set appointment?")
         userDate = input("Preferred Date: ") # to get user's preferred date
         userTime = input("Preferred Time: ") # to get user's preferred time
-        client.sched = Schedule(userDate, userTime)
-        print(client.sched)
-        print(client.sched.wholeDateTime)
+        # comparison code block should come in here
         
+        
+        # process of inserting time slot into database (once confirmed to be available)
         clientTuple = (name, age, contactNum, address, client.sched.wholeDateTime)
-        newClientSlot = timee.Time()
+        newClientSlot = timee.Time() # new instance of Time class to be inserted into database
         conn = newClientSlot.initializeDB('clients.db')
         newClientSlot.addData(conn, clientTuple)
-        
-        
-        
 
+        # confirmation of time slot reservation
+        client.sched = Schedule(userDate, userTime)
+        print(client.sched)
+
+        # generation of the qr code block should come in here
+        # https://betterprogramming.pub/how-to-generate-and-decode-qr-codes-in-python-a933bce56fd0
+        # link above might help us for this
+        
+        
+        # prompt for next action to do, exit program if "quit" criterias was satisfied
         print("\nWhat do you want to do next?")
         print("1: Appoint a checkup appointment for another patient.")
         print("2: Exit.")
